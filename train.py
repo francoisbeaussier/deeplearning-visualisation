02dcceb09c4ab9a82f05c6569acad7d1f9d68cfd
francois <francois@beaussier.net>
2020-08-17 15:21:32 +1000

    Improved visuals, using a custom decision region plot for better performance

--- train.py
+++ train.py
@@ -1,10 +1,43 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from mlxtend.plotting import plot_decision_regions
+# from mlxtend.plotting import plot_decision_regions
 
 tf.random.set_seed(1)
 np.random.seed(1)
+
+from matplotlib.colors import ListedColormap
+
+class DecisionRegionPlot():
+    def __init__(self, resolution):
+        self.resolution = resolution
+        self.initialized = False
+    
+    def setup(self, X, y):
+        self.markers = ('s', 'x', 'o', '^', 'v')
+        self.colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+        self.cmap = ListedColormap(self.colors[:len(np.unique(y))])
+
+        self.x1_min, self.x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        self.x2_min, self.x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        self.xx1, self.xx2 = np.meshgrid(
+            np.arange(self.x1_min, self.x1_max, self.resolution), 
+            np.arange(self.x2_min, self.x2_max, self.resolution))
+        self.map = np.array([self.xx1.ravel(), self.xx2.ravel()]).T
+        self.initialized = True
+ 
+    def plot(self, X, y, clf):
+        if self.initialized == False:
+            self.setup(X, y)
+
+        Z = clf.predict(self.map)
+        Z = Z.reshape(self.xx1.shape)
+        plt.contourf(self.xx1, self.xx2, Z, alpha=0.3, cmap=self.cmap)
+        plt.xlim(self.xx1.min(), self.xx1.max())
+        plt.ylim(self.xx2.min(), self.xx2.max())
+
+        for idx, cl in enumerate(np.unique(y)):
+            plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=self.colors[idx], marker=self.markers[idx], label=cl, edgecolor='black')
 
 x = np.random.uniform(low=-1, high=1, size=(200, 2))
 y = np.ones(len(x))
@@ -14,13 +47,6 @@ x_train = x[:100, :]
 y_train = y[:100]
 x_valid = x[100:, :]
 y_valid = y[100:]
-
-# fig = plt.figure(figsize=(6, 6))
-# plt.plot(x[y==0, 0], x[y==0, 1], 'o', alpha=0.75, markersize=10)
-# plt.plot(x[y==1, 0], x[y==1, 1], '^', alpha=0.75, markersize=10)
-# plt.xlabel(r'$x_1$', size=15)
-# plt.ylabel(r'$x_2$', size=15)
-# plt.show()
 
 model = tf.keras.Sequential()
 model.add(
@@ -47,41 +73,43 @@ model.add(
     
 model.summary()
 
+drp = DecisionRegionPlot(resolution=0.02)
+
 def plot_graph(model, epoch, history, x_valid, y_valid):
-    if epoch == 0:
-        return
 
-    fig = plt.figure(figsize=(16, 4))
+    fig = plt.figure(figsize=(12, 6), constrained_layout=True)
+    gs = fig.add_gridspec(12, 12)
 
-    ax = fig.add_subplot(1, 3, 1)
-    plt.plot(history['loss'], lw=4)
-    plt.plot(history['val_loss'], lw=4)
+    ax = fig.add_subplot(gs[:6, :6])
+    plt.plot(history['loss'], lw=3)
+    plt.plot(history['val_loss'], lw=3)
     plt.legend(['Train loss', 'Validation loss'], fontsize=15)
-    ax.set_xlabel('Epochs', size=15)
+    # ax.set_xlabel('Epochs', size=15)
 
-    ax = fig.add_subplot(1, 3, 2)
-    plt.plot(history['binary_accuracy'], lw=4)
-    plt.plot(history['val_binary_accuracy'], lw=4)
+    ax = fig.add_subplot(gs[6:, :6])
+    plt.plot(history['binary_accuracy'], lw=3)
+    plt.plot(history['val_binary_accuracy'], lw=3)
     plt.legend(['Train acc', 'Validation acc'], fontsize=15)
     ax.set_xlabel('Epochs', size=15)
 
-    ax = fig.add_subplot(1, 3, 3)
-    plot_decision_regions(X=x_valid, y=y_valid.astype(np.integer), clf=model, zoom_factor=1)
+    ax = fig.add_subplot(gs[:, 6:])
+    drp.plot(x_valid, y_valid.astype(np.integer), model)
     ax.set_xlim(-1.1, 1.1)
     ax.set_ylim(-1.1, 1.1)
     ax.set_xticks([-1, 0, 1])
     ax.set_yticks([-1, 0, 1])
 
     ax.set_xlabel(r'$x_1$', size=15)
-    ax.xaxis.set_label_coords(1, -0.025)
+    # ax.xaxis.set_label_coords(1, -0.025)
     ax.set_ylabel(r'$x_2$', size=15)
-    ax.yaxis.set_label_coords(-0.025, 1)
+    ax.yaxis.set_label_coords(0.05, 0.5)
 
-    plt.tight_layout(rect=[0, 0.03, 1, 0.9])
+    # plt.tight_layout(rect=[0, 0.03, 1, 0.9])
 
-    fig.suptitle('Learning XOR (Tensorflow - 3 hidden layers of 4 units each)') 
-    plt.savefig(f'xor\\training{epoch:>03d}.png')
-    plt.show()
+    fig.suptitle('Learning XOR with Tensorflow - 3 hidden layers of 4 units each', size=18)
+    plt.savefig(f'xor\\training{epoch+1:>03d}.png')
+    # plt.show()
+    fig.clear()
 
 class PlotStep(tf.keras.callbacks.Callback):
     def __init__(self, model, x_valid, y_valid):
@@ -125,11 +153,11 @@ hist = model.fit(
     y_train, 
     validation_data=(x_valid, y_valid),
     callbacks=[PlotStep(model, x_valid, y_valid)],
-    epochs=2,
+    epochs=200,
     batch_size=2
 )
 
-from mlxtend.plotting import plot_decision_regions
+# from mlxtend.plotting import plot_decision_regions
 
-history = hist.history
+# history = hist.history
 
